9ba91a7db843d327e9f86ee8fe00aca59042c2a7
camillatofani <camillatofani@gmail.com>
2023-10-18 17:18:38 +0200

    products props

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,35 +1,53 @@
 import styles from './Products.module.css'
+import { useState } from 'react'
 import SingleProduct from './../SingleProduct/SingleProduct'
 
 function Products() {
+	const [notify, setNotify] = useState(false)
+
 	const products = [
 		{
 			id: 1,
 			name: 'iPhone',
 			brand: 'Apple',
-			price: '799.90'
+			price: 799.90
 		},
 		{
 			id: 2,
 			name: 'Tastiera',
 			brand: 'Samsung',
-			price: '19.90'
+			price: 19.90
 		},
 		{
 			id: 3,
 			name: 'Nome',
 			brand: 'Samsung',
-			price: '19.90'
+			price: 19.90
 		}
 	]
 
+	function addItem(id) {
+		const item = products[id - 1];
+		const lists = JSON.parse(localStorage.getItem('cart'));
+		if (lists) {
+			localStorage.setItem('cart', JSON.stringify([...lists, item]))
+		} else {
+			localStorage.setItem('cart', JSON.stringify([item]))
+		}
+		setNotify(true)
+		setTimeout(() => setNotify(false), 2000);
+	}
+
 	return (
-		<div className={ styles.products }>
-			{
-				products.map((product) => (
-					<SingleProduct key={ product.id } id={ product.id } text={ product.name } brand={ product.brand } price={ product.price } />
-				))
-			}
+		<div>
+			<div className={ styles.products }>
+				{
+					products.map((product) => (
+						<SingleProduct addItem={ () => addItem(product.id) } key={ product.id } id={ product.id } text={ product.name } brand={ product.brand } price={ product.price } />
+					))
+				}
+			</div>
+			{ notify && <div>Add!</div> }
 		</div>
 	)
 }
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -2,13 +2,17 @@ import PropTypes from 'prop-types'
 import styles from './SingleProduct.module.css'
 import img from './../../assets/images/smartphone.jpg';
 
-function SingleProduct({id, text, brand, price}) {
+function SingleProduct({ id, text, brand, price, addItem }) {
+	function selectProduct() {
+		addItem(id)
+	}
+
 	return (
-		<div data-id={ id } className={ styles.singleProduct }>
+		<div className={ styles.singleProduct }>
 			<div>{ text }</div>
 			<div><img src={ img } /></div>
 			<div className={styles.bottom}>
-				<button className='primary'>Add</button>
+				<button data-id={ id } className='primary' onClick={selectProduct}>Add</button>
 				<div>{ brand }</div>
 				<div>{ price }€</div>
 			</div>
@@ -17,6 +21,7 @@ function SingleProduct({id, text, brand, price}) {
 }
 
 SingleProduct.propTypes = {
+	addItem: PropTypes.func,
 	id: PropTypes.number.isRequired,
 	text: PropTypes.string.isRequired,
 	brand: PropTypes.string.isRequired,
