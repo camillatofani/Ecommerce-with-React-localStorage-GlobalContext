8b7b31308e10d10ee72013126310dae9a3a63d2c
camillatofani <camillatofani@gmail.com>
2023-10-18 16:37:01 +0200

    add props in products for single product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,12 +2,34 @@ import styles from './Products.module.css'
 import SingleProduct from './../SingleProduct/SingleProduct'
 
 function Products() {
+	const products = [
+		{
+			id: 1,
+			name: 'iPhone',
+			brand: 'Apple',
+			price: '799.90'
+		},
+		{
+			id: 2,
+			name: 'Tastiera',
+			brand: 'Samsung',
+			price: '19.90'
+		},
+		{
+			id: 3,
+			name: 'Nome',
+			brand: 'Samsung',
+			price: '19.90'
+		}
+	]
+
 	return (
 		<div className={ styles.products }>
-			<SingleProduct />
-			<SingleProduct />
-			<SingleProduct />
-			<SingleProduct />
+			{
+				products.map((product) => (
+					<SingleProduct key={ product.id } id={ product.id } text={ product.name } brand={ product.brand } price={ product.price } />
+				))
+			}
 		</div>
 	)
 }
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,18 +1,27 @@
+import PropTypes from 'prop-types'
 import styles from './SingleProduct.module.css'
 import img from './../../assets/images/smartphone.jpg';
 
-function SingleProduct() {
+function SingleProduct({id, text, brand, price}) {
 	return (
-		<div className={ styles.singleProduct }>
-			<div>Nome</div>
+		<div data-id={ id } className={ styles.singleProduct }>
+			<div>{ text }</div>
 			<div><img src={ img } /></div>
 			<div className={styles.bottom}>
 				<button className='primary'>Add</button>
-				<div>Samsung</div>
-				<div>19,90€</div>
+				<div>{ brand }</div>
+				<div>{ price }€</div>
 			</div>
 		</div>
 	)
 }
+
+SingleProduct.propTypes = {
+	id: PropTypes.number.isRequired,
+	text: PropTypes.string.isRequired,
+	brand: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired
+};
+
 
 export default SingleProduct
